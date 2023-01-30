a2e71dee214b26b6b37db8ab0fd551bf07937db9
Arjunanda <vincentiusmandala@gmail.com>
2023-01-31 01:53:30 +0700

    Modify Users Table

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,14 +33,17 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'email' => 'required|string|email|max:255|unique:' . User::class,
+            'username' => 'required|string|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'username' => $request->username,
             'password' => Hash::make($request->password),
+            'profile_photo' => "https://ui-avatars.com/api/?name=" . str_replace(' ', '+', $request->name),
         ]);
 
         event(new Registered($user));
